50b6b4293e0e749b68e3f7b0549fa3c9f3f5eb40
Anirudhya <anirudhyaduyti@gmail.com>
2022-05-26 22:22:01 +0600

    mongo connect

--- index.js
+++ index.js
@@ -22,6 +22,25 @@ app.use((req, res, next) => {
     }
 });
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2bong.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const theBoltsDB = client.db('the-bolts');
+        const usersCollection = theBoltsDB.collection('users');
+
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally { }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('The Bolts Manufacturing');
 })
